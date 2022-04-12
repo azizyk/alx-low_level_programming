1b53cc27512aca02fdf63bedc4b5717381f923e4
Abdelaziz Kassaw <azizikhero@gmail.com>
2022-04-12 16:43:32 +0300

    Created all tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the string "_Putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_Putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
