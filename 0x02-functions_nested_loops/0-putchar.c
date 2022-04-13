204263fe6e2cec6bacd1653b4e3ab50dd635fe6f
Abdelaziz Kassaw <azizikhero@gmail.com>
2022-04-13 09:26:55 +0300

    modified 0-putchar.c & 7-print_last_digit.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * main - prints the string "_Putchar" 
- * Return: 0 on success.
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-      _putchar('_');
-      _putchar('p');
-      _putchar('u');
-      _putchar('t');
-      _putchar('c');
-      _putchar('h');
-      _putchar('a');
-      _putchar('r');
-      return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,19 @@
 #include "main.h"
-/**
- * print_last_digit - prints the last digit of a number.
- * @n: digit to find the last place of.
- * Return: The last digit.
- */
 
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
 int print_last_digit(int n)
 {
+	int last_digit = n % 10;
 
-n = n >= 0 ? n : n * -1;
-int last = n % 10;
-_putchar('0' + last);
+	if (last_digit < 0)
+		last_digit *= -1;
 
-return (last);
+	_putchar(last_digit + '0');
 
+	return (last_digit);
 }
