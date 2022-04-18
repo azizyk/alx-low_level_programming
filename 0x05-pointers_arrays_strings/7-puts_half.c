28ebfec4c2ab6e3b09bd4eef44c5155cc5f5cda0
Abdelaziz Kassaw <azizikhero@gmail.com>
2022-04-18 16:50:41 +0300

    modified 7-put_hat.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,27 +8,27 @@
 
 void puts_half(char *str)
 {
-	int c, a;
-	char l;
+	int len, n, i;
 
-	for (c = 0; str[c] != 0; c++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
+		len++;
 	}
-	if (c % 2 == 0)
+
+	if (len % 2 == 0)
 	{
-		for ((a = c / 2); str[a] != 0; a++)
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			l = str[a];
-			_putchar(l);
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
 		}
 	}
-	else
-        {
-                for ((a = (c - 1) / 2); str[a] != 0; a++)
-                {
-                        l = str[a];
-                        _putchar(l);
-                }
-        }
 	_putchar('\n');
 }
