572e8b5b140828c59116483e6a39b25918bac8f3
Abdelaziz Kassaw <azizikhero@gmail.com>
2022-04-25 16:18:43 +0300

    Updated 0-memset.c & 2-strchr.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * memset - Fills the first n bytes of the memory area
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strchr - returns a pointer to the first occurence of the
  * character c in the string s
@@ -6,6 +7,7 @@
  * @s: string to search character
  * Return: pointer to first occurence of character c or null if not found
  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
